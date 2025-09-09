92463e4c46ca0d2dee76f379230f2dc9243f18d2
polyfloyd <floyd@polyfloyd.net>
2025-09-09 21:03:35 +0200

    ci: Re-introduce the bump workflow

--- flake.nix
+++ flake.nix
@@ -2,21 +2,26 @@
   inputs = {
     # Pinned to Rust v1.70
     nixpkgs.url = "github:nixos/nixpkgs?ref=50a7139fbd1acd4a3d4cfa695e694c529dd26f3a";
+    nixpkgs-cz.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      nixpkgs-cz,
+    }:
     {
       devShells = builtins.mapAttrs (system: pkgs: {
         default = pkgs.mkShell {
           packages = with pkgs; [
             cargo
             clippy
-            commitizen
             ffmpeg
             gcc
             rust-analyzer
             rustfmt
+            nixpkgs-cz.legacyPackages.${system}.commitizen
           ];
         };
       }) nixpkgs.legacyPackages;
